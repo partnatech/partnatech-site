985fede70f8466f5bb5e038f8d26a820c35ae873
Jessy Hanifiah <72622302+jeeehaan@users.noreply.github.com>
2024-03-07 14:59:31 +0700

    feat: add homepage (#1)
    * feat: add homepage
    
    * feat: data engineer hero
    
    * add i18n example
    
    ---------
    
    Co-authored-by: = <=>

---
+++ src/components/isomorphic/navbar.tsx
@@ -0,0 +1,134 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { Menu } from "lucide-react";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { cn } from "@/utils/cn";
+import { Button } from "@/components/ui/button";
+
+interface Link {
+  label: string;
+  href: {
+    pathname: string;
+    id: string;
+  };
+}
+
+const links = [
+  {
+    label: "About",
+    href: {
+      pathname: "/about",
+      id: "#",
+    },
+  },
+  {
+    label: "Programs",
+    href: {
+      pathname: "/programs",
+      id: "#programs",
+    },
+  },
+  {
+    label: "Business",
+    href: {
+      pathname: "/business",
+      id: "#business",
+    },
+  },
+  {
+    label: "Blog",
+    href: {
+      pathname: "/blog",
+      id: "#blog",
+    },
+  },
+  {
+    label: "Event",
+    href: {
+      pathname: "/events",
+      id: "#",
+    },
+  },
+];
+
+function pathCheck(pathname: string, obj: Link) {
+  return pathname === obj.href.pathname ? obj.href.id : obj.href.pathname + obj.href.id;
+}
+
+export default function Navbar() {
+  const pathname = usePathname();
+
+  return (
+    <div className={`min-w-screen sticky top-0 z-20 backdrop-blur-lg backdrop-filter`}>
+      <div className="container mx-auto grid grid-cols-2 lg:grid-cols-3 items-center py-6 px-4">
+        <Logo />
+        <NavigationMenu className="hidden lg:flex">
+          <NavigationMenuList>
+            {links.map((link, idx) => (
+              <NavigationMenuItem key={idx}>
+                <NavigationMenuLink href={pathCheck(pathname, link)} className={navigationMenuTriggerStyle()}>
+                  {link.label}
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+        <div className="flex gap-1 justify-self-end">
+          <MobileNav />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <Link href="/">
+      <div className="font-black hover:text-primary text-2xl transition-colors ease-linear duration-300">
+        PartnaLearn
+      </div>
+    </Link>
+  );
+}
+
+function MobileNav() {
+  const pathname = usePathname();
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild className="lg:hidden">
+        <Button variant={"ghost"} size={"icon"} onClick={() => setOpen(true)}>
+          <Menu className="w-6 h-6" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="w-48 pt-10">
+        <div className="flex flex-col gap-1">
+          {links.map((link, idx) => (
+            <Button
+              variant={"ghost"}
+              size={"lg"}
+              key={idx}
+              className={cn("justify-start px-0 font-bold")}
+              onClick={() => setOpen(false)}
+              asChild
+            >
+              <Link href={pathCheck(pathname, link)}>{link.label}</Link>
+            </Button>
+          ))}
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
---
+++ src/utils/i18n/home.ts
@@ -0,0 +1,4 @@
+export const homeCopy = {
+  headline:
+    "Loreit am ipsum, dolor smet consectetur adipisicing elit. Molestias fugit inventore mollitia vero tempora reprehenderit repellendus vel id explicabo reiciendis!",
+};
