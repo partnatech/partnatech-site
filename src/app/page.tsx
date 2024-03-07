921c25fc108c02005fa67ca22b58f43c31e979f2
= <=>
2024-03-07 15:56:04 +0800

    add i18n example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import { ProgramList } from "@/components/bootcamp/program-list";
 import { InfiniteMovingCardsDemo } from "@/components/review/student-testimonials";
-
-import Image from "next/image";
+import { homeCopy } from "@/utils/i18n/home";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section className="flex max-w-6xl">
         <div>
-          <h1 className="text-2xl mb-8">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias fugit inventore mollitia vero tempora reprehenderit repellendus vel id explicabo reiciendis!</h1>
+          <h1 className="text-2xl mb-8">{homeCopy.headline}</h1>
           <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
             <span className="absolute inset-0 overflow-hidden rounded-full">
               <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
@@ -16,7 +15,13 @@ export default function Home() {
             <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
               <span>Lihat program</span>
               <svg fill="none" height="16" viewBox="0 0 24 24" width="16" xmlns="http://www.w3.org/2000/svg">
-                <path d="M10.75 8.75L14.25 12L10.75 15.25" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" />
+                <path
+                  d="M10.75 8.75L14.25 12L10.75 15.25"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                />
               </svg>
             </div>
             <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
