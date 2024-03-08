be63f5dece900d8080c95429a726f52f268b2715
Jessy Hanifiah <72622302+jeeehaan@users.noreply.github.com>
2024-03-08 22:34:10 +0700

    feat: add responsiveness (#6)
    * feat: add responsiveness
    
    * feat: adjust responsive
    
    * feat: adjust heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,13 @@
 import { ProgramList } from "@/components/home/program-list";
 import HomeHero from "@/components/home/hero";
-import { InfiniteMovingCardsDemo } from "@/components/home/student-testimonials";
+import { StudentTestimonials } from "@/components/home/student-testimonials";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-24 py-8 space-y-16">
-      <section>
-        <HomeHero />
-      </section>
-      <section>
-        <ProgramList />
-      </section>
-      <section>
-        <h1 className="text-3xl text-center mb-4">Student Testimonials</h1>
-        <InfiniteMovingCardsDemo />
-      </section>
-    </main>
+    <div className="lg:space-y-16 space-y-8 p-4">
+      <HomeHero />
+      <ProgramList />
+      <StudentTestimonials />
+    </div>
   );
 }
