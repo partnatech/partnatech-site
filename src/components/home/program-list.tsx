8613ab9a214e31e0edff9a5d81677a91f6a45e7c
Jessy Hanifiah <jessyhanifiah@gmail.com>
2024-03-08 22:30:40 +0700

    feat: adjust heading

--- src/components/home/program-list.tsx
+++ src/components/home/program-list.tsx
@@ -9,7 +9,7 @@ export const ProgramList = () => {
       <h1 className="text-3xl text-center mb-4" id="programs">
         Our Programs
       </h1>
-      <div className="grid grid-cols-1 lg:grid-cols-2 lg:max-w-2xl max-w-xs gap-16 mx-auto">
+      <div className="grid grid-cols-1 lg:grid-cols-2 lg:max-w-3xl max-w-xs gap-16 mx-auto">
         {homeCopy.programs.map((program, index) => {
           return <ProgramCard key={index} name={program.name} duration={program.duration} link={program.link} />;
         })}
