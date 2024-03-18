6f76d6e99a6c5300cc579f57e45dfc9e403e7b3a
mctn6 <130217944+mctn6@users.noreply.github.com>
2024-03-18 22:49:13 +0700

    add GA and fix seo (#27)
    * add GA and fix seo
    
    * set dark mode as default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import Navbar from "@/components/shared/navbar";
 import Footer from "@/components/shared/footer";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,10 +11,15 @@ export const metadata: Metadata = {
   title: "Partnatech | Building bridge between talent and the industry",
   description: "Learn in-demand data skills and engineering expertise to land your dream industry role.",
   twitter: {
-    title: 'Partnatech',
-    description: 'Building bridge between talent and the industry',
-    card: 'summary_large_image',
-    images: ['https://yt3.googleusercontent.com/REh5Y-tz0JzQc6Te0wO7vrJB85aEmv_wEGU3sqtuEg1w7IphfZTrUI2F3aVVky7EDwHxiUhhD24=s160-c-k-c0x00ffffff-no-rj'], // Must be an absolute URL
+    title: "Partnatech",
+    description: "Building bridge between talent and the industry",
+    card: "summary_large_image",
+    images: [
+      "https://yt3.googleusercontent.com/REh5Y-tz0JzQc6Te0wO7vrJB85aEmv_wEGU3sqtuEg1w7IphfZTrUI2F3aVVky7EDwHxiUhhD24=s160-c-k-c0x00ffffff-no-rj",
+    ],
+  },
+  alternates: {
+    canonical: `/`,
   },
 };
 
@@ -25,6 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <GoogleTagManager gtmId="G-KD885ZC352" />
         <Navbar />
         {children}
         <Footer />
--- src/components/analytics-engineering/hero.tsx
+++ src/components/analytics-engineering/hero.tsx
@@ -25,9 +25,9 @@ export default function AnalyticsEngineeringHero() {
       </div>
       <div className="mx-auto max-w-7xl px-6 pb-24 sm:pb-32 lg:flex lg:px-8">
         <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
-          <h1 className="mt-10 text-3xl font-bold tracking-tight text-white sm:text-5xl">
+          <h2 className="mt-10 text-3xl font-bold tracking-tight text-white sm:text-5xl">
             {analyticsEngineeringCopy.title}
-          </h1>
+          </h2>
           <p className="mt-6 text-lg leading-8 text-gray-300">
             {analyticsEngineeringCopy.description}
           </p>
