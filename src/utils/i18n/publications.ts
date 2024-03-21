6e47f887b24f69422e2c9282c247c9d088006aaa
Jessy Hanifiah <jessyhanifiah@gmail.com>
2024-03-21 10:38:50 +0700

    feat: add videos

--- src/utils/i18n/publications.ts
+++ src/utils/i18n/publications.ts
@@ -1,16 +1,47 @@
 export const publicationsCopy = {
   videos: [
     {
-      id: 1,
+      title: "Workshop airflow-dbt-integration Day 3",
+      href: "https://www.youtube.com/watch?v=dmJTeSofskA",
+      embedUrl: "https://www.youtube.com/embed/dmJTeSofskA?si=n6_3xPz3AkYzPxwk",
+      description:
+        "Integrating Airflow and dbt for Data Pipeline Building is a workshop designed to equip participants with the skills and knowledge necessary to construct robust data pipelines using two of the most powerful tools in the data engineering ecosystem: Apache Airflow and dbt (data build tool)",
+      date: "March 14th, 2024",
+      datetime: "2024-03-14",
+      categories: [{ title: "Data Analytics" }, { title: "Analytics Engineering" }],
+    },
+    {
+      title: "Workshop airflow-dbt-integration Day 2",
+      href: "https://www.youtube.com/watch?v=7jWYSEgVaVY&t=7s",
+      embedUrl: "https://www.youtube.com/embed/7jWYSEgVaVY?si=sV7fH7Mk3Sq5Ipku",
+      description:
+        "Integrating Airflow and dbt for Data Pipeline Building is a workshop designed to equip participants with the skills and knowledge necessary to construct robust data pipelines using two of the most powerful tools in the data engineering ecosystem: Apache Airflow and dbt (data build tool)",
+      date: "March 13th, 2024",
+      datetime: "2024-03-13",
+      categories: [{ title: "Data Analytics" }, { title: "Analytics Engineering" }],
+    },
+    {
+      title: "Workshop airflow-dbt-integration Day 1",
+      href: "https://www.youtube.com/watch?v=nqb2sd8s33E&t=97s",
+      embedUrl: "https://www.youtube.com/embed/nqb2sd8s33E?si=oEzWQ8E1y4mjHsH5",
+      description:
+        "Integrating Airflow and dbt for Data Pipeline Building is a workshop designed to equip participants with the skills and knowledge necessary to construct robust data pipelines using two of the most powerful tools in the data engineering ecosystem: Apache Airflow and dbt (data build tool)",
+      date: "March 12th, 2024",
+      datetime: "2024-03-14",
+      categories: [{ title: "Data Analytics" }, { title: "Analytics Engineering" }],
+    },
+    {
       title: "Working as a Head of Data Analytics",
-      href: "https://www.youtube.com/watch?v=nqb2sd8s33E",
+      href: "https://www.youtube.com/watch?v=nnaYrucSqDU",
+      embedUrl: "https://www.youtube.com/embed/nnaYrucSqDU?si=ZIo9HjshPbM7Sg27",
       description:
         "Discover the challenges, strategies, and triumphs of steering data-driven initiatives, and gain invaluable insights for success in this pivotal role.",
       date: "March 7th, 2024",
-      datetime: "2020-03-16",
-      category: { title: "Data Analytics" },
+      datetime: "2024-03-07",
+      categories: [{ title: "Data Analytics" }],
     },
   ],
+
   blogs: [
     {
       id: 1,
