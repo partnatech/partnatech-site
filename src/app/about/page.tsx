0d9da29cd291454c38b317f64349494acc34f114
Jessy Hanifiah <jessyhanifiah@gmail.com>
2024-03-26 15:01:12 +0700

    feat: add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="mx-auto max-w-sm lg:max-w-7xl">
+      <h2 className="mt-12 text-center text-4xl font-bold">About</h2>
+      <div className="space-y-4 pt-6 text-lg text-gray-300">
+        <p>
+          partna.tech develops data talents, provides data talents, and builds & does research about
+          data engineering and data science best-practices in Indonesia. we assemble scalable
+          infrastructure and environment for data talents to practice their skills.
+        </p>
+        <p>
+          With collective experience in Data Science, Data Engineering, and Artificial Intelligence,
+          our team ensures you will find the best data engineers the state-of-the-art solution
+          related to data engineering practice and problems.
+        </p>
+      </div>
+      <div className="mt-12">
+        <h3 className="mb-1 text-2xl">Our Vision</h3>
+        <p className="text-lg text-gray-300">
+          To tackle the practical experience gap of data talents, providing a &quot;playing
+          ground&quot; and the best mentors in the field.
+        </p>
+      </div>
+      <div className="mt-12">
+        <h3 className="mb-1 text-2xl">Our Mission</h3>
+        <p className="text-lg text-gray-300">
+          To build practical infrastructures for data talents and potential data talents for
+          practicing and creating valuable portfolios, we also ensure potential data talents achieve
+          their career goals with end-to-end practice.
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/components/scholarship/step.tsx
+++ src/components/scholarship/step.tsx
@@ -8,23 +8,18 @@ export const ScholarshipStep = () => {
   const steps = [
     {
       name: "Isi Form",
-      status: "complete",
     },
     {
       name: "Seleksi Porfolio dan Profile",
-      status: "complete",
     },
     {
       name: "Tanda Tangan Kontrak",
-      status: "complete",
     },
     {
       name: "Pembayaran Deposit Fee",
-      status: "complete",
     },
     {
-      name: "Onboading",
-      status: "complete",
+      name: "Onboarding",
     },
   ];
 
@@ -39,45 +34,22 @@ export const ScholarshipStep = () => {
                 key={step.name}
                 className={classNames(stepIdx !== steps.length - 1 ? "pb-10" : "", "relative")}
               >
-                {step.status === "complete" ? (
-                  <>
-                    {stepIdx !== steps.length - 1 ? (
-                      <div
-                        className="absolute left-4 top-4 -ml-px mt-0.5 h-full w-0.5 bg-primary"
-                        aria-hidden="true"
-                      />
-                    ) : null}
-                    <div className="group relative flex items-start">
-                      <span className="flex h-9 items-center">
-                        <span className="relative z-10 flex h-8 w-8 items-center justify-center rounded-full bg-primary group-hover:bg-teal-500">
-                          {stepIdx + 1}
-                        </span>
-                      </span>
-                      <span className="ml-4 flex min-w-0 flex-col">
-                        <span className="text-sm font-medium">{step.name}</span>
-                      </span>
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    {stepIdx !== steps.length - 1 ? (
-                      <div
-                        className="absolute left-4 top-4 -ml-px mt-0.5 h-full w-0.5 bg-gray-300"
-                        aria-hidden="true"
-                      />
-                    ) : null}
-                    <div className="group relative flex items-start">
-                      <span className="flex h-9 items-center" aria-hidden="true">
-                        <span className="relative z-10 flex h-8 w-8 items-center justify-center rounded-full border-2 border-gray-300 bg-white group-hover:border-gray-400">
-                          <span className="h-2.5 w-2.5 rounded-full bg-transparent group-hover:bg-gray-300" />
-                        </span>
-                      </span>
-                      <span className="ml-4 flex min-w-0 flex-col">
-                        <span className="text-sm font-medium text-gray-500">{step.name}</span>
-                      </span>
-                    </div>
-                  </>
-                )}
+                {stepIdx !== steps.length - 1 ? (
+                  <div
+                    className="absolute left-4 top-4 -ml-px mt-0.5 h-full w-0.5 bg-primary"
+                    aria-hidden="true"
+                  />
+                ) : null}
+                <div className="group relative flex items-start">
+                  <span className="flex h-9 items-center">
+                    <span className="relative z-10 flex h-8 w-8 items-center justify-center rounded-full bg-primary group-hover:bg-teal-500">
+                      {stepIdx + 1}
+                    </span>
+                  </span>
+                  <span className="ml-4 flex min-w-0 flex-col">
+                    <span className="text-sm font-medium">{step.name}</span>
+                  </span>
+                </div>
               </li>
             ))}
           </ol>
