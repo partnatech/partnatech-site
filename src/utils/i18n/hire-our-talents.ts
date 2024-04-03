9b5566d809f579fb6d94716e0bac4e1562c3da95
Jessy Hanifiah <jessyhanifiah@gmail.com>
2024-04-03 14:08:39 +0700

    feat: add hire talents page

---
+++ src/utils/i18n/hire-our-talents.ts
@@ -0,0 +1,29 @@
+export const hireCopy = {
+  benefits: [
+    {
+      item: "Industry-Ready Professionals",
+      description:
+        "Our graduates emerge from an intensive learning environment where they tackle real-world data challenges and develop solutions that align with industry standards.",
+    },
+    {
+      item: "Adaptable and Collaborative",
+      description:
+        "Our talents are trained to seamlessly integrate into diverse working environments, readily adapt to evolving data landscapes, and collaborate effectively with cross-functional teams.",
+    },
+    {
+      item: "Continous Learning Ethic",
+      description:
+        "They possess a continuous learning mindset, always staying abreast of industry trends and technological advancements.",
+    },
+    {
+      item: "Diverse Perpective",
+      description:
+        "Our bootcamp attracts aspiring data engineers from various backgrounds, contributing to a rich, inclusive learning environment. ",
+    },
+    {
+      item: "Hiring Made Easy",
+      description:
+        "We understand that finding the right talent can be time-consuming. That’s why we offer a streamlined hiring process to connect you with our top graduates.",
+    },
+  ],
+};
